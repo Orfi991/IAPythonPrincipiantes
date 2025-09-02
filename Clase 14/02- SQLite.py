9afaa8bcb10760c8825c4bfd203d41d4d8cd625d
Orfilia Moreira <orfiliamorei991@gmail.com>
2025-09-02 15:31:50 -0300

    Ejercicios clase 14

--- Clase 14/02- SQLite.py
+++ Clase 14/02- SQLite.py
@@ -4,34 +4,34 @@ import sqlite3
 conn = sqlite3.connect("escuela.db")
 cursor = conn.cursor()
 
-# 2. Crear tabla
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS estudiantes (
-    id INTEGER PRIMARY KEY,
-    nombre TEXT,
-    edad INTEGER,
-    curso TEXT
-)
-""")
+# # 2. Crear tabla
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS estudiantes (
+#     id INTEGER PRIMARY KEY,
+#     nombre TEXT,
+#     edad INTEGER,
+#     curso TEXT
+# )
+# """)
 
-# 3. Insertar datos 
-cursor.executemany("""
-INSERT INTO estudiantes (id, nombre, edad, curso)
-VALUES (?, ?, ?, ?)
-""", [
-    (1, 'Ana', 20, 'Matemáticas'),
-    (2, 'Juan', 22, 'Historia'),
-    (3, 'Maria', 21, 'Física')
-])
+# # 3. Insertar datos 
+# cursor.executemany("""
+# INSERT INTO estudiantes (id, nombre, edad, curso)
+# VALUES (?, ?, ?, ?)
+# """, [
+#     (1, 'Ana', 20, 'Matemáticas'),
+#     (2, 'Juan', 22, 'Historia'),
+#     (3, 'Maria', 21, 'Física')
+# ])
 
 # # 4. Consultas
-# print("Todos los estudiantes:")
-# for fila in cursor.execute("SELECT * FROM estudiantes"):
-#     print(fila)
+print("Todos los estudiantes:")
+for fila in cursor.execute("SELECT * FROM estudiantes"):
+    print(fila)
 
-# print("\nMayores de 20:")
-# for fila in cursor.execute("SELECT * FROM estudiantes WHERE edad > 20"):
-#     print(fila)
+print("\nMayores de 20:")
+for fila in cursor.execute("SELECT * FROM estudiantes WHERE edad > 20"):
+    print(fila)
 
 # # 5. Actualizar
 # cursor.execute("UPDATE estudiantes SET curso = 'Literatura' WHERE nombre = 'Juan'")
